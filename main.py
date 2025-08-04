a5b06389fd0a834acb52329a85400dc9527e1c94
techy4shri <sushri4tech@gmail.com>
2025-08-04 23:25:49 +0530

    added main logic file, the initialization and wrote some imports, along with app exec

--- main.py
+++ main.py
@@ -7,5 +7,38 @@ Kindly do not touch unless understanding the consequences.
 
 import os
 import sys
+import shutil
+import qdarktheme
+from typing import Optional
+from pathlib import Path
 import logging
-import PyQt6
+from PyQt6.QtCore import (
+    QStringListModel,
+    QSortFilterProxyModel,
+    Qt,
+    QRegularExpression,
+    QThreas,
+    QUrl
+)
+from PyQt6.QtGui import QRegularExpressionValidator
+from PyQt6.Widgets import QApplication, QMainWindow, QMessagebox, QFileDialog, QTextBrowser
+
+#main logic for application starts from here tbh
+
+
+class Application(QMainWindow):
+    """
+    This class handles all communication with the .ui file,
+    all lambdas go here.
+    """
+    def __init__(self):
+        super().__init__()
+        #will add logic as i go
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    setup_logging()
+    window=Application()
+    window.show()
+    sys.exit(app.exec())
